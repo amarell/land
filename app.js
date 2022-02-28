4e31cf935385e05073b38f8e6f355e38a0c45c9e
amarell <mujagicamar@gmail.com>
2022-02-28 12:11:11 +0100

    small screen projects

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-console.log("Hello connected")
-
 let apiPrefix = "https://api.github.com"
 
 let projectContainer = document.getElementsByClassName("projects-container")
+let ssProjectContainer = document.getElementsByClassName("ss-projects-container")
 
 
 async function getProjects() {
@@ -15,40 +14,58 @@ async function getProjects() {
 
     const data = await res.json();
 
-    console.log(data);
-    console.log(data.length)
-
-
-
     for(let i = 0; i<data.length; i++) {
-        console.log(data[i])
         if(data[i].name == "amarell") { continue; }
         let project = document.createElement("div")
+        let ssProject = document.createElement("div")
 
         project.className = "project"
-
+        ssProject.className = "ss-project"
+    
         let folderIcon = document.createElement("i")
+        let ssFolderIcon = document.createElement("i");
         folderIcon.className = "fas fa-folder-open"
+        ssFolderIcon.className = "fas fa-folder-open"
 
         folderIcon.onclick = function visitLink() {
             window.location = data[i].html_url;
         }
+
+        ssFolderIcon.onclick = function visitLink() {
+            window.location = data[i].html_url;
+        }
+
+
     
         let projectTitle = document.createElement("h2");
         projectTitle.innerHTML = data[i].name;
+
+        let ssProjectTitle = document.createElement("h2");
+        ssProjectTitle.innerHTML = data[i].name;
 
         let projectDescription = document.createElement("p");
         projectDescription.innerHTML =  data[i].description ? 
                                         '<i class="fas fa-chevron-right"></i>' + "  " + data[i].description : 
                                         "Currently without description"
 
-        project.appendChild(folderIcon)
+        let ssProjectDescription = document.createElement("p");
+        ssProjectDescription.innerHTML = data[i].description ? 
+                                         '<i class="fas fa-chevron-right"></i>' + "  " + data[i].description : 
+                                         "Currently without description"
+
+
+        project.appendChild(folderIcon);
         project.appendChild(projectTitle);
         project.appendChild(projectDescription);
 
-        projectContainer[0].appendChild(project)
+        ssProject.appendChild(ssFolderIcon)
+        ssProject.appendChild(ssProjectTitle)
+        ssProject.appendChild(ssProjectDescription)
         
+        projectContainer[0].appendChild(project);
+        ssProjectContainer[0].appendChild(ssProject)
     }
+    console.log(projectContainer)
 }
 
 getProjects();
